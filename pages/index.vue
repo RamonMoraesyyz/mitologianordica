605dc53ef683ed26874a9d67e06b225374793b62
RamonMoraesyyz <ramon.k13@gmail.com>
2024-02-10 22:06:42 -0300

    versão embrionária projeto

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="bg-brown-dark min-h-screen flex flex-col justify-center items-center text-white">
+    <h1 class="text-5xl font-bold mb-9 text-center">Mitologia Grega</h1>
+
+    <p class="text-lg mb-8 text-center">O conhecimento da mitologia grega é crucial para compreender a sociedade atual, pois ela serve como uma base cultural e literária influente, permeando a arte, a literatura e a linguagem contemporâneas. 
+      As referências mitológicas encontradas em expressões comuns e conceitos universais nos ajudam a interpretar melhor a nossa cultura e a entender temas como ética, moralidade e heroísmo. 
+      Além disso, os mitos gregos oferecem insights sobre as origens da civilização ocidental e fornecem reflexões significativas sobre a natureza humana e os desafios enfrentados pela sociedade moderna.
+      O objetivo do projeto é ser um catálogo da mitologia grega com breves descrições dos personagens e para ilustrar, imagens geradas por IA.</p>
+
+    <p class="text-lg mb-8 text-center font-bold">Primeira geração de deuses, os olimpianos, filhos dos titãs Crono e Réia:</p>
+    <div class="flex flex-wrap justify-center">
+      <nuxt-link to="/zeus">
+        <button class="bg-yellow-500 hover:bg-yellow-600 text-black font-bold py-2 px-4 rounded mr-4 mb-4">Zeus</button>
+      </nuxt-link>
+      <nuxt-link to="/héstia">
+        <button class="bg-yellow-500 hover:bg-yellow-600 text-black font-bold py-2 px-4 rounded mr-4 mb-4">Héstia</button>
+      </nuxt-link>
+      <nuxt-link to="/démeter">
+        <button class="bg-yellow-500 hover:bg-yellow-600 text-black font-bold py-2 px-4 rounded mr-4 mb-4">Démeter</button>
+      </nuxt-link>
+      <nuxt-link to="/hera">
+        <button class="bg-yellow-500 hover:bg-yellow-600 text-black font-bold py-2 px-4 rounded mr-4 mb-4">Hera</button>
+      </nuxt-link>
+      <nuxt-link to="/hades">
+        <button class="bg-yellow-500 hover:bg-yellow-600 text-black font-bold py-2 px-4 rounded mr-4 mb-4">Hades</button>
+      </nuxt-link>
+      <nuxt-link to="/poseidon">
+        <button class="bg-yellow-500 hover:bg-yellow-600 text-black font-bold py-2 px-4 rounded mr-4 mb-4">Poseidon</button>
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
+
+<style scoped>
+.bg-brown-dark {
+  background-color: #211512; 
+}
+</style>
---
+++ pages/zeus.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="relative bg-brown-dark text-white flex flex-col md:flex-row min-h-screen overflow-hidden">
+  
+      <!-- Conteúdo esquerdo (imagem) -->
+      <div class="w-full md:w-1/2">
+        <img src="../public/zeus.jpg" class="w-full h-auto md:h-full object-cover transform scale-70" />
+      </div>
+  
+      
+      <div class="w-full md:w-1/2 p-4 md:p-8 flex flex-col justify-center">
+  
+     
+        <h1 class="text-2xl md:text-4xl font-bold mb-4 md:mb-10 mt-1 text-center">Zeus</h1>
+  
+        <div class="mb-4 text-center">
+        <label class="mb-2">Zeus, o principal deus do panteão grego, é reconhecido como o soberano do céu e governante dos deuses e dos homens e tem como símbolo o raio. Filho de Cronos e Réia, ele liderou a rebelião contra seu pai e os Titãs na Titanomaquia, emergindo vitorioso. Representado como um homem maduro de barba e postura majestosa, Zeus é frequentemente retratado empunhando um raio, símbolo de seu poder e autoridade.
+        <p class="mb-2">Como deus do trovão e do céu, ele controlava os elementos naturais e garantia a ordem cósmica, intervindo para assegurar que a justiça fosse cumprida. Conhecido por seus numerosos amores e aventuras, Zeus tinha uma inclinação por se relacionar com mortais, resultando em uma infinidade de filhos semideuses ou divinos, como Atena, Hermes e Perseu, entre muitos outros.</p>
+        <p class="mb-2">Além disso, Zeus era mestre dos disfarces, frequentemente assumindo diferentes formas para seduzir suas amantes ou para interagir com humanos, o que lhe permitia escapar de situações complicadas e cumprir seus desejos e objetivos divinos.</p> </label>
+        </div>
+
+        <div class="mt-4 text-center">
+        <nuxt-link to="/">
+          <button class="bg-yellow-500 hover:bg-yellow-600 text-black font-bold py-2 px-4 rounded">
+             Menu
+          </button>
+        </nuxt-link>
+      </div>
+        
+      </div>
+    </div>
+  </template>
+
+<style scoped>
+.bg-brown-dark {
+  background-color: #211512; 
+}
+</style>
