309c5aa311edb4961a7b4c19fe3e5b2823c43b73
RamonMoraesyyz <ramon.k13@gmail.com>
2024-03-24 19:24:48 -0300

    App.vue

--- app.vue
+++ app.vue
@@ -1,26 +1,11 @@
 <template>
     <div>
-      <!-- Aqui você pode adicionar elementos que estarão presentes em todas as páginas, como um cabeçalho -->
-      <header>
-        <!-- Conteúdo do cabeçalho -->
-      </header>
+
   
       <!-- Renderiza o conteúdo da página atual -->
       <NuxtPage />
   
-      <!-- Aqui você pode adicionar elementos que estarão presentes em todas as páginas, como um rodapé -->
-      <footer>
-        <!-- Conteúdo do rodapé -->
-      </footer>
+ 
     </div>
   </template>
   
-  <script>
-  export default {
-    // Configurações do componente, se necessário
-  }
-  </script>
-  
-  <style>
-  /* Estilos específicos para o layout global podem ser adicionados aqui */
-  </style>
