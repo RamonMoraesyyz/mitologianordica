b7582f2416eefb73c5df4c996cf06ee8ef8498c4
Eduardo Braga <eduardocbraga@hotmail.com>
2024-03-24 21:04:14 -0300

    converte imagens para png

--- pages/oceanicos/hera.vue
+++ pages/oceanicos/hera.vue
@@ -1,45 +1,67 @@
 <template>
-    <div class="relative bg-white text-black flex flex-col md:flex-row min-h-screen overflow-hidden">
-  
-      <!-- Conteúdo esquerdo (imagem) -->
-      <div class="w-full h-screen md:w-1/2 md:h-auto overflow-hidden">
-        <img src="/hera.jpg" class="w-full h-full object-cover" />
+  <div
+    class="relative bg-white text-black flex flex-col md:flex-row min-h-screen overflow-hidden"
+  >
+    <!-- Conteúdo esquerdo (imagem) -->
+    <div class="w-full h-screen md:w-1/2 md:h-auto overflow-hidden">
+      <img src="/hera.png" class="w-full h-full object-cover" />
+    </div>
+
+    <div class="w-full md:w-1/2 p-4 md:p-8 flex flex-col justify-center">
+      <h1 class="text-2xl md:text-4xl font-bold mb-8 md:mb-10 mt-1 text-center">
+        Hera
+      </h1>
+
+      <div class="mb-4 text-center">
+        <label class="mb-2 text-xl">
+          <p>
+            Hera é uma das principais deusas da mitologia grega, sendo a irmã e
+            esposa de Zeus, o rei dos deuses. Ela é considerada a deusa do
+            casamento, da maternidade e da família, e é frequentemente retratada
+            como uma figura majestosa e poderosa. Hera era filha de Cronos e
+            Reia e, como muitos dos deuses gregos, foi criada no Monte Olimpo.
+            Sua união com Zeus foi uma das mais importantes do panteão grego,
+            embora fosse frequentemente marcada por conflitos e ciúmes. De
+            acordo com a mitologia, Zeus a traiu repetidamente com outras
+            deidades e mortais, o que provocou a ira de Hera e resultou em
+            muitos episódios dramáticos. Hera era protetora do casamento e da
+            fidelidade conjugal, e punia severamente tanto as amantes de Zeus
+            quanto os filhos ilegítimos que ele gerava. Ela era conhecida por
+            sua natureza ciumenta e vingativa, frequentemente direcionando sua
+            ira contra aqueles que cruzavam seu caminho ou desrespeitavam sua
+            autoridade. Além de seu papel como esposa de Zeus, Hera também era
+            uma deusa importante por direito próprio. Ela era considerada a
+            rainha do Olimpo e uma figura materna para muitos dos outros deuses.
+            Era comum invocar sua proteção em questões relacionadas ao
+            casamento, parto e vida familiar. Na arte e na literatura grega,
+            Hera é frequentemente retratada como uma mulher majestosa e
+            imponente, vestida com roupas elegantes e adornada com coroas e
+            jóias. Ela é muitas vezes representada segurando um cetro ou uma
+            romã, símbolo de fertilidade e prosperidade. Apesar de seu
+            temperamento e das dificuldades em seu relacionamento com Zeus, Hera
+            era reverenciada e adorada em toda a Grécia Antiga, com templos e
+            rituais dedicados a ela em muitas cidades. Ela era uma figura
+            central na religião e na cultura gregas, e sua influência perdurou
+            por séculos após o declínio do mundo antigo.
+          </p>
+        </label>
       </div>
-  
-      
-      <div class="w-full md:w-1/2 p-4 md:p-8 flex flex-col justify-center">
-  
-     
-        <h1 class="text-2xl md:text-4xl font-bold mb-8 md:mb-10 mt-1 text-center">Hera</h1>
-  
-        <div class="mb-4 text-center">
-        <label class="mb-2 text-xl"> <p>Hera é uma das principais deusas da mitologia grega, sendo a irmã e esposa de Zeus, o rei dos deuses. Ela é considerada a deusa do casamento, da maternidade e da família, e é frequentemente retratada como uma figura majestosa e poderosa. Hera era filha de Cronos e Reia e, como muitos dos deuses gregos, foi criada no Monte Olimpo.
 
-        Sua união com Zeus foi uma das mais importantes do panteão grego, embora fosse frequentemente marcada por conflitos e ciúmes. De acordo com a mitologia, Zeus a traiu repetidamente com outras deidades e mortais, o que provocou a ira de Hera e resultou em muitos episódios dramáticos.
-
-        Hera era protetora do casamento e da fidelidade conjugal, e punia severamente tanto as amantes de Zeus quanto os filhos ilegítimos que ele gerava. Ela era conhecida por sua natureza ciumenta e vingativa, frequentemente direcionando sua ira contra aqueles que cruzavam seu caminho ou desrespeitavam sua autoridade.
-
-        Além de seu papel como esposa de Zeus, Hera também era uma deusa importante por direito próprio. Ela era considerada a rainha do Olimpo e uma figura materna para muitos dos outros deuses. Era comum invocar sua proteção em questões relacionadas ao casamento, parto e vida familiar.
-
-        Na arte e na literatura grega, Hera é frequentemente retratada como uma mulher majestosa e imponente, vestida com roupas elegantes e adornada com coroas e jóias. Ela é muitas vezes representada segurando um cetro ou uma romã, símbolo de fertilidade e prosperidade.
-
-        Apesar de seu temperamento e das dificuldades em seu relacionamento com Zeus, Hera era reverenciada e adorada em toda a Grécia Antiga, com templos e rituais dedicados a ela em muitas cidades. Ela era uma figura central na religião e na cultura gregas, e sua influência perdurou por séculos após o declínio do mundo antigo.</p> </label>
-        </div>
-
-        <div class="mt-4 text-center">
+      <div class="mt-4 text-center">
         <nuxt-link to="/">
-          <button class="bg-zinc-300 hover:bg-zinc-400 text-black font-bold py-2 px-4 rounded">
-             Menu
+          <button
+            class="bg-zinc-300 hover:bg-zinc-400 text-black font-bold py-2 px-4 rounded"
+          >
+            Menu
           </button>
         </nuxt-link>
       </div>
-        
-      </div>
     </div>
-  </template>
+  </div>
+</template>
 
 <style scoped>
 .bg-brown-dark {
-  background-color: #211512; 
+  background-color: #211512;
 }
 </style>
--- pages/oceanicos/poseidon.vue
+++ pages/oceanicos/poseidon.vue
@@ -1,49 +1,76 @@
 <template>
-    <div class="relative bg-white text-black flex flex-col md:flex-row min-h-screen overflow-hidden">
-  
-      <!-- Conteúdo esquerdo (imagem) -->
-      <div class="w-full md:w-1/2 flex items-center justify-center">
-        <img src="/poseidon.jpg" class="w-full h-screen md:h-auto object-cover" />
+  <div
+    class="relative bg-white text-black flex flex-col md:flex-row min-h-screen overflow-hidden"
+  >
+    <!-- Conteúdo esquerdo (imagem) -->
+    <div class="w-full md:w-1/2 flex items-center justify-center">
+      <img src="/poseidon.png" class="w-full h-screen md:h-auto object-cover" />
+    </div>
+
+    <div class="w-full md:w-1/2 p-4 md:p-8 flex flex-col justify-center">
+      <h1 class="text-2xl md:text-4xl font-bold mb-8 md:mb-10 mt-1 text-center">
+        Poseidon
+      </h1>
+
+      <div class="mb-4 text-center">
+        <label class="mb-2 text-xl">
+          <p>
+            Poseidon é uma das divindades mais importantes na mitologia grega,
+            sendo conhecido como o deus dos mares, das tempestades, dos
+            terremotos e dos cavalos. Ele é frequentemente retratado como uma
+            figura poderosa e imponente, associada ao vasto e imprevisível
+            oceano. Poseidon era filho dos titãs Cronos e Reia, e irmão de Zeus
+            e Hades, fazendo parte da segunda geração de deuses olímpicos.
+
+            <br />
+
+            Como senhor dos mares, Poseidon tinha controle sobre as águas e era
+            frequentemente retratado carregando um tridente, seu símbolo
+            distintivo e uma arma temida. Ele era adorado por marinheiros e
+            pescadores, que o invocavam para garantir viagens seguras e boas
+            pescarias, mas também temido por sua capacidade de provocar
+            tempestades e maremotos. Além de seu domínio sobre os oceanos,
+            Poseidon também era conhecido por sua rivalidade com seu irmão Zeus.
+            Ambos disputaram o domínio do Monte Olimpo e do mundo dos mortais, e
+            Poseidon frequentemente desafiava a autoridade de Zeus, resultando
+            em conflitos e disputas entre os deuses.
+            <br />
+
+            Poseidon teve vários filhos e filhas em suas muitas aventuras
+            amorosas, incluindo criaturas mitológicas como o cavalo alado Pégaso
+            e o ciclope Polifemo. Seus descendentes também incluem heróis como
+            Teseu, que foi criado em parte pelo deus do mar. Na arte e na
+            literatura grega, Poseidon é frequentemente retratado como um homem
+            imponente e barbudo, vestindo uma túnica e segurando seu tridente.
+            Ele é muitas vezes representado cavalgando pelas ondas em um carro
+            puxado por cavalos marinhos.
+            <br />
+
+            Apesar de sua personalidade tempestuosa e suas frequentes disputas
+            com outros deuses, Poseidon era uma figura reverenciada na Grécia
+            Antiga, com templos e festivais dedicados a ele em várias cidades
+            costeiras. Sua influência perdurou por séculos após o declínio da
+            cultura grega, e ele continua sendo uma das figuras mais
+            emblemáticas e poderosas da mitologia.
+          </p>
+        </label>
       </div>
-  
-      <div class="w-full md:w-1/2 p-4 md:p-8 flex flex-col justify-center">
-  
-        <h1 class="text-2xl md:text-4xl font-bold mb-8 md:mb-10 mt-1 text-center">Poseidon</h1>
-  
-        <div class="mb-4 text-center">
-          <label class="mb-2 text-xl">
-            <p>Poseidon é uma das divindades mais importantes na mitologia grega, sendo conhecido como o deus dos mares, das tempestades, dos terremotos e dos cavalos. Ele é frequentemente retratado como uma figura poderosa e imponente, associada ao vasto e imprevisível oceano. Poseidon era filho dos titãs Cronos e Reia, e irmão de Zeus e Hades, fazendo parte da segunda geração de deuses olímpicos.
 
-              <br>
-
-            Como senhor dos mares, Poseidon tinha controle sobre as águas e era frequentemente retratado carregando um tridente, seu símbolo distintivo e uma arma temida. Ele era adorado por marinheiros e pescadores, que o invocavam para garantir viagens seguras e boas pescarias, mas também temido por sua capacidade de provocar tempestades e maremotos.
-
-            Além de seu domínio sobre os oceanos, Poseidon também era conhecido por sua rivalidade com seu irmão Zeus. Ambos disputaram o domínio do Monte Olimpo e do mundo dos mortais, e Poseidon frequentemente desafiava a autoridade de Zeus, resultando em conflitos e disputas entre os deuses.
-            <br>
-
-            Poseidon teve vários filhos e filhas em suas muitas aventuras amorosas, incluindo criaturas mitológicas como o cavalo alado Pégaso e o ciclope Polifemo. Seus descendentes também incluem heróis como Teseu, que foi criado em parte pelo deus do mar.
-
-            Na arte e na literatura grega, Poseidon é frequentemente retratado como um homem imponente e barbudo, vestindo uma túnica e segurando seu tridente. Ele é muitas vezes representado cavalgando pelas ondas em um carro puxado por cavalos marinhos.
-            <br>
-
-            Apesar de sua personalidade tempestuosa e suas frequentes disputas com outros deuses, Poseidon era uma figura reverenciada na Grécia Antiga, com templos e festivais dedicados a ele em várias cidades costeiras. Sua influência perdurou por séculos após o declínio da cultura grega, e ele continua sendo uma das figuras mais emblemáticas e poderosas da mitologia.</p>
-          </label>
-        </div>
-  
-        <div class="mt-4 text-center">
-          <nuxt-link to="/">
-            <button class="bg-zinc-300 hover:bg-zinc-400 text-black font-bold py-2 px-4 rounded">
-              Menu
-            </button>
-          </nuxt-link>
-        </div>
-  
+      <div class="mt-4 text-center">
+        <nuxt-link to="/">
+          <button
+            class="bg-zinc-300 hover:bg-zinc-400 text-black font-bold py-2 px-4 rounded"
+          >
+            Menu
+          </button>
+        </nuxt-link>
       </div>
     </div>
-  </template>
+  </div>
+</template>
   
   <style scoped>
-    .bg-brown-dark {
-      background-color: #211512;
-    }
-  </style>
+.bg-brown-dark {
+  background-color: #211512;
+}
+</style>
