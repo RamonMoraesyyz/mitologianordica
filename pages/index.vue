f61cdc1c2beeeffe81e0c8c8f5c9d793c3a916cb
Eduardo Braga <eduardocbraga@hotmail.com>
2024-03-24 21:10:23 -0300

    Corrige responsividade

--- pages/index.vue
+++ pages/index.vue
@@ -6,14 +6,14 @@ export default {
 
 <template>
   <div class="relative bg-white text-black flex flex-col md:flex-row min-h-screen overflow-hidden">
-    <div class="flex flex-col items-center w-1/2 px-8 py-11">
+    <div class="flex flex-col items-center w-full md:w-1/2 px-8 py-11">
 
       <h1 class="text-7xl text-center">Mitologia Grega</h1>
 
       <img src="/index.png" alt="Índice" class="mb-8">
     </div>
 
-    <div class="flex flex-col justify-center items-center w-1/2 px-12 py-12">
+    <div class="flex flex-col justify-center items-center w-full md:w-1/2 px-12 py-12">
       <p class="text-2xl mb-7 mt-18">O conhecimento da mitologia grega é crucial para compreender a sociedade atual, pois
         ela serve como uma base cultural e literária influente, permeando a arte, a literatura e a linguagem
         contemporâneas.
